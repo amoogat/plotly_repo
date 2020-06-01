e7ace0ca918969f4b665e94a6c62f4e36529e6f5
Aarian Moogat <amoogat@gmail.com>
2020-06-01 00:00:59 -0700

    added interactive charts

--- plots.js
+++ plots.js
@@ -1,44 +1,4 @@
-/** GREEK DATA */
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
-
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
-
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
-
-/** DEMOGRAPHIC INFO */
-// handles initial display case for Demographic Info
+// handles initial display case
 function init() {
   var selector = d3.select("#selDataset");
 
@@ -77,5 +37,69 @@ function buildMetadata(sample) {
     PANEL.append("h6").text("LOCATION: " + result.location);
     PANEL.append("h6").text("BBTYPE: " + result.bbtype);
     PANEL.append("h6").text("WFREQ: " + result.wfreq);
+  });
+}
+
+/** BUILDS BAR CHART */
+function buildCharts(sample){
+  d3.json("samples.json").then((data) => {
+    var sampleData = data.samples;
+    var resultArray = sampleData.filter(obj => obj.id == sample);
+    var result = resultArray[0];
+    var yAxis = result.otu_ids.slice(0,10);
+    var metadata = data.metadata;
+    var resultArray2 = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result2 = resultArray2[0]; 
+
+    var trace1 = {
+      x: result.sample_values,
+      y: yAxis,
+      text: result.otu_labels,
+      name: "OTU",
+      type: "bar",
+      orientation: "h"
+    };
+    var inf1 = [trace1];
+    var layout1 = {
+      title: "OTU",
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+      }
+    };
+
+    var trace2 = {
+      x: result.otu_ids,
+      y: result.sample_values,
+      mode: 'markers',
+      marker: {
+        color: result.otu_ids,
+        size: result.sample_values
+      }
+    };
+    var inf2 = [trace2];
+    var layout2 = {
+      title: 'OTU',
+      showlegend: false,
+      width: 1200,
+      height: 600
+    };
+
+    var data3 = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: result2.wfreq,
+        title: { text: "Belly Button Washing Frequency" },
+        type: "indicator",
+        mode: "gauge+number"
+      }
+    ];
+    var layout3 = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+
+    Plotly.newPlot('gauge', data3, layout3);
+    Plotly.newPlot("bar", inf1, layout1);
+    Plotly.newPlot("bubble", inf2, layout2);
   });
 }
